94c499b72f9c51b49fae8db63096c2fb703f0c03
Rahul Shaw <rahulshaw903866@gmail.com>
2025-04-19 19:04:48 +0530

    Added To-do app using only js

---
+++ TO -DO APP/app.js
@@ -0,0 +1,35 @@
+let todo =[]
+let req = prompt("Please enter your request ")
+console.log(req)
+while(true){
+    if((req == "quit") || (req == null)){
+        console.log("Quitting app.... ")
+        break;
+    }
+    if(req == "list"){
+        for (i=0; i<todo.length; i++)
+        console.log(i, todo[i])
+        console.log("---------")
+    }
+    else if(req == "add"){
+        let task =  prompt("Please enter the task you want to add ")
+        todo.push(task)
+        for (elements of todo){
+            console.log(`Task:(${elements}) is added`)
+            console.log("---------")
+        }
+        
+    }
+    else if (req == "delete"){
+        let idx =  prompt("Please enter the task index ")
+        todo.splice(idx,1)
+        console.log("Task deleted")
+    }
+    else{
+        console.log("wrong input")
+    }
+    req = prompt("Please enter your request ")
+   
+
+
+}
