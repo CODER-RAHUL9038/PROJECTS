b0015e30cc198427ca69eb5ad7f7d65826de2d5b
Rahul Shaw <rahulshaw903866@gmail.com>
2025-08-30 23:03:45 +0530

    fixed display overflow bug

--- calculator/script.js
+++ calculator/script.js
@@ -12,8 +12,9 @@ for (let btn of btns) {
       str = str.slice(0, -1);
       display.innerText = str;
     } else if (e.target.innerText == "AC") {
-      str = "00";
+      str = "";
       display.innerText = str;
+      display.style.fontSize = "2rem";
     } else if (e.target.innerText == "=") {
       try {
         str = str
@@ -29,12 +30,25 @@ for (let btn of btns) {
       }
     } else {
       // No consecutive operator logic
+
       if (
         operators.includes(e.target.innerText) &&
         operators.includes(str.slice(-1))
       ) {
         return;
       }
+      // More than 30 Characters
+      if (display.innerText.length > 28) {
+        alert("Maximum Character Exceeds!");
+        return;
+      }
+      if (display.innerText.length > 15) {
+        display.style.fontSize = "1.5rem"; // shrink font
+      }
+      if (display.innerText.length > 18) {
+        display.style.fontSize = "1.1rem"; // shrink further
+      }
+
       str += e.target.innerText;
       display.innerText = str;
     }
