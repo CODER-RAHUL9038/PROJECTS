aaa1a290ab6ac0c73a3bea76e495eb5f26dc78e7
Rahul Shaw <rahulshaw903866@gmail.com>
2025-08-30 23:41:11 +0530

    fixed keydown max char bug

--- calculator/script.js
+++ calculator/script.js
@@ -6,53 +6,68 @@ const operators = ["+", "-", "*", "/", "÷", "x"];
 
 let str = "";
 
-// For keydown
+// For keydown feature
 document.addEventListener("keydown", function (e) {
-    let key = e.key;
+  let key = e.key;
+
+  // allow numbers 0-9
+  if (key >= "0" && key <= "9") {
     
+    // More than 30 Characters
+    if (display.innerText.length > 28) {
+      alert("Maximum Character Exceeds!");
+      return;
+    }
+    if (display.innerText.length > 15) {
+      display.style.fontSize = "1.5rem"; // shrink font
+    }
+    if (display.innerText.length > 18) {
+      display.style.fontSize = "1.1rem"; // shrink further
+    }
 
-    // allow numbers 0-9
-    if (!isNaN(key)) {
-       str += key;
-       console.log(str)
+    str += key;
+
+    display.innerText = str;
+  }
+
+  // allow operators
+  if (["+", "-", "*", "/"].includes(key)) {
+    if (operators.includes(key) && operators.includes(str.slice(-1))) {
+      return;
+    }
+    str += key;
+    display.innerText = str;
+  }
+
+  // allow Enter for "="
+  if (key === "Enter") {
+    try {
+      str = str
+        .replace(/\b0+(\d+)/g, "$1")
+        .replace(/x/g, "*")
+        .replace(/÷/g, "/");
+      str = eval(str).toString();
       display.innerText = str;
-    }
-
-    // allow operators
-    if (["+", "-", "*", "/"].includes(key)) {
-      str += key
-      display.innerText = str;
-    }
-
-    // allow Enter for "="
-    if (key === "Enter") {
-      try {
-        str = str
-          .replace(/\b0+(\d+)/g, "$1")
-          .replace(/x/g, "*")
-          .replace(/÷/g, "/");
-        str = eval(str).toString();
-        display.innerText = str;
-      } catch (error) {
-        display.innerText = "Error";
-        str = "";
-        console.log(error);
-      }
-    }
-
-    // allow Backspace for delete
-    if (key === "Backspace") {
-      str = str.slice(0, -1);
-      display.innerText = str;
-    }
-
-    // allow Escape to clear
-    if (key === "Escape") {
+    } catch (error) {
+      display.innerText = "Error";
       str = "";
-      display.innerText = str;
-      display.style.fontSize = "2rem";
+      console.log(error);
     }
-  });
+  }
+
+  // allow Backspace for delete
+  if (key === "Backspace") {
+    str = str.slice(0, -1);
+    display.innerText = str;
+  }
+
+  // allow Escape to clear
+  if (key === "Escape") {
+    str = "";
+    display.innerText = str;
+    display.style.fontSize = "2rem";
+  }
+});
 
 // For mouse and touch
 for (let btn of btns) {
